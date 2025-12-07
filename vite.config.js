489f0a6f3dc8699231e8d2d067fe8cded47b420f
SanjeetCamper <sanjeethelpline@gmail.com>
2025-12-07 13:46:29 +0530

    cache purge build

--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,47 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import { VitePWA } from "vite-plugin-pwa";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     react(),
-    // VitePWA({
-    //   registerType: "autoUpdate",
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
 
-    //   workbox: {
-    //     cleanupOutdatedCaches: true, // 👈 सबसे जरूरी
-    //   },
+      workbox: {
+        cleanupOutdatedCaches: true, // 👈 सबसे जरूरी
+      },
 
-    //   includeAssets: [
-    //     "logo.png",
-    //     "bg_img.png",
-    //     "hand_wave.png",
-    //     "header_img.png",
-    //     "favicon.ico",
-    //   ],
+      includeAssets: [
+        "logo.png",
+        "bg_img.png",
+        "hand_wave.png",
+        "header_img.png",
+        "favicon.ico",
+      ],
 
-    //   manifest: {
-    //     name: "Sanjeet Water Supplier",
-    //     short_name: "SWS",
-    //     start_url: "/",
-    //     display: "standalone",
-    //     background_color: "#ffffff", // splash background
-    //     theme_color: "#a5f3fc", // light aqua
-    //     icons: [
-    //       {
-    //         src: "logo.png",
-    //         sizes: "192x192",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "logo.png",
-    //         sizes: "512x512",
-    //         type: "image/png",
-    //       },
-    //     ],
-    //   },
-    // }),
+      manifest: {
+        name: "Sanjeet Water Supplier",
+        short_name: "SWS",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff", // splash background
+        theme_color: "#a5f3fc", // light aqua
+        icons: [
+          {
+            src: "logo.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "logo.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
   ],
 });
